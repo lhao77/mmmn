65d9b7570139ddc8a59383636cba1f2e51bf3971
lhao77 <lhao77@gmail.com>
2012-01-12 11:10:45 +0800

    New first

---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  simp
+//
+//  Created by hao luo on 12-1-11.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
